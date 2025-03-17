9769f850c561714290fa142b47d9903c78fdf049
emguse <30695541+emguse@users.noreply.github.com>
2025-03-17 13:11:02 +0000

    initial commit

---
+++ rect-draw/src/components/Canvas.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-container class="canvas-container">
+      <Rect 
+        v-for="n in count" 
+        :key="n" 
+        width="100" 
+        height="100" 
+        color="blue" 
+      />
+    </v-container>
+  </template>
+  
+  <script setup lang="ts">
+  import Rect from './Rect.vue';
+  import { ref } from 'vue';
+  
+  const count = ref(5); // rect count
+  </script>
+  
+  <style scoped>
+  .canvas-container {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    gap: 5px;
+  }
+  </style>
---
+++ rect-draw/src/components/Rect.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="rect-container">
+    <v-sheet :class="['rect-box']" :style="sheetStyle" elevation="2" theme="light"></v-sheet>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue';
+
+export default defineComponent({
+  name: 'Rect',
+  props: {
+    width: {
+      type: [Number, String] as PropType<number | string>,
+      default: 200
+    },
+    height: {
+      type: [Number, String] as PropType<number | string>,
+      default: 200
+    },
+    color: {
+      type: String,
+      default: '#2196f3'
+    }
+  },
+  computed: {
+    sheetStyle(): Record<string, string> {
+      const widthValue = typeof this.width === 'number' ? `${this.width}px` : `${parseInt(this.width as string, 10)}px`;
+      const heightValue = typeof this.height === 'number' ? `${this.height}px` : `${parseInt(this.height as string, 10)}px`;
+      return {
+        width: widthValue,
+        height: heightValue,
+        backgroundColor: this.color
+      };
+    }
+  }
+});
+</script>
+
+<style scoped>
+.rect-container {
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  width: auto;
+  height: auto;
+}
+
+.rect-box {
+  display: block;
+}
+</style>
